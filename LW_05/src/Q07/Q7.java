28b3420fb60bb4be741ba7398b6c46f226efb4a6
senuda-git <dilmith.senu@gmail.com>
2025-05-22 04:47:49 +0530

    Update Q7.java

--- LW_05/src/Q07/Q7.java
+++ LW_05/src/Q07/Q7.java
@@ -2,19 +2,23 @@ package Q07;
 
 import java.util.Scanner;
 
-public class Q7 {
+public class Q07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //User sentence input
         System.out.println("Enter a sentence:");
         String sentence = scanner.nextLine();
 
+        //User replacing word input
         System.out.println("Enter the word to be replaced:");
         String wordToReplace = scanner.next();
 
+        //User replacement word input
         System.out.println("Enter the replacement word:");
         String replacementWord = scanner.next();
 
+        //dividing into words - neglect spaces
         String[] words = sentence.split(" ");
 
         for (int i = 0; i < words.length; i++) {
@@ -24,14 +28,7 @@ public class Q7 {
         }
 
         // Rebuild sentence using StringBuilder
-        StringBuilder modifiedSentenceBuilder = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            modifiedSentenceBuilder.append(words[i]);
-            if (i < words.length - 1) {
-                modifiedSentenceBuilder.append(" ");
-            }
-        }
-        String modifiedSentence = modifiedSentenceBuilder.toString();
+        String modifiedSentence = String.join(" ", words);
 
         System.out.println("Modified sentence:");
         System.out.println(modifiedSentence);
